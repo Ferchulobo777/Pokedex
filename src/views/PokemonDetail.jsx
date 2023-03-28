76abfe14dabc6f3f3ea124a63072f66b195ac67c
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-03-28 18:56:15 -0300

    update

--- src/views/PokemonDetail.jsx
+++ src/views/PokemonDetail.jsx
@@ -12,38 +12,90 @@ const getPokemonById = async (id) => {
   }
 };
 
+const getPokemonNameById = async (id) => {
+  try {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    return res.data.names.find((name) => name.language.name === 'en').name;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getPokemonStatsById = async (id) => {
+  try {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    return res.data.stats;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const PokemonDetail = () => {
   const { id } = useParams();
   const [pokemon, setPokemon] = useState(null);
+  const [pokemonName, setPokemonName] = useState(null);
+  const [stats, setStats] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getPokemonById(id);
+      const name = await getPokemonNameById(id);
+      const stats = await getPokemonStatsById(id);
       setPokemon(data);
+      setPokemonName(name);
+      setStats(stats);
       setLoading(false);
     };
     fetchData();
   }, [id]);
 
+  const tiposPokemonEspanol = {
+    normal: 'Normal',
+    fighting: 'Lucha',
+    flying: 'Volador',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    rock: 'Roca',
+    bug: 'Bicho',
+    ghost: 'Fantasma',
+    steel: 'Acero',
+    fire: 'Fuego',
+    water: 'Agua',
+    grass: 'Planta',
+    electric: 'Eléctrico',
+    psychic: 'Psíquico',
+    ice: 'Hielo',
+    dragon: 'Dragón',
+    dark: 'Siniestro',
+    fairy: 'Hada',
+  };
+
+  const calculateStatPercentage = (statValue) => {
+    const maxStatValue = 255; // Máximo valor de estadística de cualquier Pokémon
+    return (statValue / maxStatValue) * 100;
+  };
+
   return (
     <section className="flex justify-center w-full">
       <header
-        className={`bg bg${pokemon?.types[0].type.name} flex flex-col justify center border-2 card rounded-xl mt-48 mb-60 w-5/6 h-screen`}
+        className={`bg bg${pokemon?.types[0].type.name} flex flex-col justify center border-2 card rounded-xl mt-48 mb-20 w-5/6 h-full`}
       >
-        <h1 className="text-center mt-8">Pokemon Detail</h1>
-
+        <h1 className="text-4xl text-center mt-8 font-black flex justify-center">
+          {pokemonName}
+        </h1>
+        <p
+          className="text-center mt-4 mb-16 text-xl font-bold bg-green-500 w-20 h-10 flex justify-center items-center justify-self-center rounded-md hover:saturate-200"
+          style={{ margin: 'auto', marginBottom: '65px', marginTop: '40px' }}
+        >
+          N° {id}{' '}
+        </p>
         <motion.div
-          className={`bg bg${pokemon?.types[0].type.name} card w-96 h-96 flex hover:saturate-200 hover:rounded-full hover:transform hover:scale-125 mt-12 justify-center items-center`}
+          className={`bg bg${pokemon?.types[0].type.name} card w-96 h-96 flex hover:saturate-200 hover:rounded-full hover:transform hover:scale-125 mt-32 justify-center items-center`}
           style={{ margin: 'auto' }}
         >
           {loading ? (
-            <img
-              className=""
-              src={`../assets/img/pokeball.png`}
-              className="loader"
-              alt="loader"
-            />
+            <img src={`../assets/img/pokeball.png`} className="loader" alt="loader" />
           ) : (
             <motion.img
               layout
@@ -57,7 +109,44 @@ const PokemonDetail = () => {
             />
           )}
         </motion.div>
-        <p className="text-center mt-2">N° {id} </p>
+        <hr className="w-3/4 bg-black flex justify-center items-center mx-auto mt-6" />
+        <p className="text-center mt-4 mb-4 font-bold text-2xl">Tipo</p>
+        <div className="flex justify-center text-xl font-bold mb-8 mt-2">
+          {pokemon?.types.map((type) => (
+            <div
+              key={type.slot}
+              className={`bg bg${pokemon?.types[0].type.name} card w-28 h-16 flex hover:saturate-200 justify-center items-center mx-6`}
+            >
+              {tiposPokemonEspanol[type.type.name]}
+            </div>
+          ))}
+        </div>
+        <hr className="w-3/4 bg-black flex justify-center items-center mx-auto" />
+        <div>
+          <h2 className="text-center mt-4 font-bold text-2xl mb-4">Habilidades</h2>
+          <div className="flex flex-wrap justify-center">
+            {pokemon?.abilities.map((ability) => (
+              <p
+                key={ability.ability.name}
+                className="bg-gray-200 px-3 py-1 m-1 rounded-full font-bold"
+              >
+                {ability.ability.name}
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <hr className="w-3/4 bg-black flex justify-center items-center mx-auto mt-10" />
+          <h2 className="text-center mt-4 mb-4 font-bold text-2xl">Estadisticas</h2>
+
+          <ul className="text-center font-bold text-xl mb-2 flex justify-center flex-col capitalize">
+            {stats?.map((stat, index) => (
+              <li className="mt-2 mb-2" key={index}>
+                {stat.stat.name}: {stat.base_stat}
+              </li>
+            ))}
+          </ul>
+        </div>
       </header>
     </section>
   );
