94cd36e622d0ccd515cb0d0746e904a012d9efe0
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-04-19 10:43:15 -0300

    update

--- src/views/Pokedex.jsx
+++ src/views/Pokedex.jsx
@@ -2,13 +2,13 @@ import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../context/UserContext';
 import axios from 'axios';
 import PokemonCard from '../components/PokemonCard';
-import ByType from '../components/ByType';
 import { usePagination } from '../hooks/usePagination';
 
 const Pokedex = () => {
   const { user } = useContext(UserContext);
   const [pokemons, setPokemons] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedType, setSelectedType] = useState('');
   const [setLoading] = useState(false);
   const pokemonsPagination = usePagination(
     pokemons.length > 0
@@ -44,13 +44,20 @@ const Pokedex = () => {
 
   const loadAllPokemons = async () => {
     const allPokemons = await getAllPokemons();
-
     setPokemons(allPokemons);
+  };
+  const loadPokemonsByType = async () => {
+    if (selectedType) {
+      const pokemonsByType = await getByType(selectedType);
+      setPokemons(pokemonsByType);
+    } else {
+      loadAllPokemons();
+    }
   };
 
   useEffect(() => {
-    loadAllPokemons();
-  }, []);
+    loadPokemonsByType();
+  }, [selectedType]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
@@ -59,7 +66,9 @@ const Pokedex = () => {
   const handleResetSearch = () => {
     setSearchTerm('');
   };
-
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+  };
   return (
     <div className="flex flex-col justify-center items-center">
       <p className="w-full relative text-center top-52 text-black font-bold text-4xl light">
@@ -88,7 +97,69 @@ const Pokedex = () => {
           Borrar
         </button>
       </div>
-      <ByType getByType={getByType} />
+      <div className="mt-5 mb-5 w-full mx-auto flex justify-center items-center">
+        <select
+          className="w-full md:w-1/2 text-center text-gray-500 h-12 text-lg rounded-lg input p-3 placeholder:text-gray-400 font-black placeholder:text-center border border-black"
+          value={selectedType}
+          onChange={handleTypeChange}
+        >
+          <option className="font-black text-black-300" value="">Selecciona un tipo</option>
+          <option value="normal" className="font-black text-rose-300">
+            Normal
+          </option>
+          <option value="fire" className="font-black text-red-500">
+            Fuego
+          </option>
+          <option value="water" className="font-black text-blue-500">
+            Agua
+          </option>
+          <option value="electric" className="font-black text-yellow-300">
+            Eléctrico
+          </option>
+          <option value="grass" className="font-black text-teal-500">
+            Planta
+          </option>
+          <option value="ice" className="font-black text-cyan-500">
+            Hielo
+          </option>
+          <option value="fighting" className="font-black text-rose-900">
+            Lucha
+          </option>
+          <option value="poison" className="font-black text-violet-500">
+            Veneno
+          </option>
+          <option value="ground" className="font-black text-orange-900">
+            Tierra
+          </option>
+          <option value="flying" className="font-black text-blue-300">
+            Volador
+          </option>
+          <option value="psychic" className="font-black text-pink-700">
+            Psíquico
+          </option>
+          <option value="bug" className="font-black text-green-500">
+            Bicho
+          </option>
+          <option value="rock" className="font-black text-gray-500">
+            Roca
+          </option>
+          <option value="ghost" className="font-black text-fuchsia-400">
+            Fantasma
+          </option>
+          <option value="dragon" className="font-black text-blue-900">
+            Dragón
+          </option>
+          <option value="dark" className="font-black text-zinc-900">
+            Siniestro
+          </option>
+          <option value="steel" className="font-black text-zinc-600">
+            Acero
+          </option>
+          <option value="fairy" className="font-black text-pink-400">
+            Hada
+          </option>
+        </select>
+      </div>
       <div className="flex flex-wrap flex-row gap-4 mt-20 justify-center w-3/4 text-xl font-bold hover:shadow-md hover:shadow-red-500 rounded-lg cursor-pointer">
         {pokemonsPagination.pages.map((page) => (
           <button
